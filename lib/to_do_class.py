4c01e8d5f5a4b0c6524868797ae696cbc31b1cf8
Sarah Mathieson <sarahmathieson@nationwide.co.uk>
2025-04-14 16:25:40 +0100

    added_a_task

--- lib/to_do_class.py
+++ lib/to_do_class.py
@@ -1,3 +1,8 @@
-class ToDoList):
-    def __init__(self, task):
-        self.task = task
+class ToDoList():
+    def __init__(self):
+        self.tasks = []
+
+    def my_list(self, task):
+        self.tasks.append(task)
+        return task
+        
--- tests/test_to_do_class.py
+++ tests/test_to_do_class.py
@@ -1,6 +1,11 @@
 from lib.to_do_class import *
 
 # add 'to do' tasks to empty list
+def test_add_function():
+    add = ToDoList()
+    result = add.my_list("plan dinner")
+    assert result == "plan dinner"
+    assert "plan dinner" in add.tasks
 
 # add 'to do' task to list with existing items
 
